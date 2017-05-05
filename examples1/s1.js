b0ab7bdc00c32549174ef4b214c1edd9b62747ac
11x256 <ahesham93@gmail.com>
2017-05-06 01:57:10 +0200

    Create s1.js

---
+++ examples1/s1.js
@@ -0,0 +1,12 @@
+console.log("Script loaded successfully ");
+Java.perform(function x(){
+    //get a wrapper for our class
+    var my_class = Java.use("com.example.a11x256.frida_test.my_activity");
+    //replace the original implmenetation of the function `fun` with our custom function
+    my_class.fun.implementation = function(x,y){
+    //print the original arguments
+    console.log( "original call: fun("+ x + ", " + y + ")");
+    //call the original implementation of `fun` with args (2,5)
+    var ret_value = this.fun(2,5);
+    return ret_value;
+    }});
