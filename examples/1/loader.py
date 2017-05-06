288db7cf00acc92f4e21e67d46d0177a8012681f
11x256 <ahesham93@gmail.com>
2017-05-06 16:37:58 +0200

    better file handling

--- examples/1/loader.py
+++ examples/1/loader.py
@@ -7,7 +7,8 @@ pid = device.spawn(["com.example.a11x256.frida_test"])
 device.resume(pid)
 time.sleep(1)  # Without it Java.perform silently fails
 session = device.attach(pid)
-script = session.create_script(open("s1.js").read())
+with open("s1.js") as f:
+	script = session.create_script(f.read())
 script.load()
 
 #prevent the python script from terminating
